a5e84a2680b0c12a7045cbb3d8f59a14770563e4
andrew <andrewasare98@gmail.com>
2021-03-24 13:45:01 +0000

    functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+# What is a function
+# Why functions
+
+# How to create a function
+# Syntax to create a funtion: def is Python keyword that tells the interpreter that the function is being declared
+# Let's create one def name_of_the_function():
+# Greeting function first iteration
+
+def greetings():
+    print("Welcome on board ")
+greetings()
